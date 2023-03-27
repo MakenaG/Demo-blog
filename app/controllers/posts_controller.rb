ad30410b341f9963899adbd664013fa36cc1fada
MakenaG <makenaglory44@gmail.com>
2023-03-27 06:07:20 +0300

    edited posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
     # before_action :find_post, only: [:show, :update, :edit, :destroy]
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
 
    def index
        @posts = Post.all.order("created_at DESC")
@@ -50,6 +51,11 @@ class PostsController < ApplicationController
        params.require(:post).permit(:title, :content)
    end
 
+   private
+
+def set_post
+  @post = Post.find(params[:id])
+end
    # def find_post
    # 	@post = Post.find(params[:id])
    # end
